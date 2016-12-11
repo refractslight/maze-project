2d221249717dae24477ec5af03c0e32efb025287
Susie <susie.odneal@gmail.com>
2016-12-10 22:39:11 -0600

    Updated waypoints, began working on coin script.

--- Maze Project/Assets/Udacity/Scripts/Coin.cs
+++ Maze Project/Assets/Udacity/Scripts/Coin.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Coin : MonoBehaviour 
+public class Coin : MonoBehaviour
+
+
 {
     //Create a reference to the CoinPoofPrefab
 
+	public GameObject CoinPoofPrefab;  
+
     public void OnCoinClicked() {
         // Instantiate the CoinPoof Prefab where this coin is located
+		Object.Instantiate(CoinPoofPrefab);
         // Make sure the poof animates vertically
         // Destroy this coin. Check the Unity documentation on how to use Destroy
     }

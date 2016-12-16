fdcecc2babb0d17185d81442463d5f5c1d66ab50
susu <susie.odneal@wpengine.com>
2016-12-15 21:55:30 -0600

    Added some code to key.cs
    can't figure out how to rotate poof yet tho

--- Maze Project/Assets/Udacity/Scripts/Coin.cs
+++ Maze Project/Assets/Udacity/Scripts/Coin.cs
@@ -8,23 +8,21 @@ public class Coin : MonoBehaviour
 {
 
 
-    //Create a reference to the CoinPoofPrefab
+	//Create a reference to the CoinPoofPrefab
 
 	public GameObject coinPoofPrefab;
 	public AudioClip coinSound;
 
 	void Update(){
-	
-		transform.position = new Vector3 (transform.position.x, 2.5f + Mathf.Sin (Time.time), transform.position.z);
 	}
-    public void OnCoinClicked() {
-        // Instantiate the CoinPoof Prefab where this coin is located
+	public void OnCoinClicked() {
+		// Instantiate the CoinPoof Prefab where this coin is located
 		// Make sure the poof animates vertically
 		Instantiate(coinPoofPrefab, transform.position, Quaternion.identity);
 
-        // Destroy this coin. Check the Unity documentation on how to use Destroy
+		// Destroy this coin. Check the Unity documentation on how to use Destroy
 		Destroy(gameObject);
 
-    }
+	}
 
 }
--- Maze Project/Assets/Udacity/Scripts/Key.cs
+++ Maze Project/Assets/Udacity/Scripts/Key.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Key : MonoBehaviour 
+
 {
     //Create a reference to the KeyPoofPrefab and Door
+	public GameObject keyPoof;
+	public GameObject door;
 
 	void Update()
 	{
 		//Bonus: Key Animation
+		transform.position = new Vector3 (transform.position.x, 2.5f + Mathf.Sin (Time.time), transform.position.z);
+
 	}
 
 	public void OnKeyClicked()
 	{
         // Instatiate the KeyPoof Prefab where this key is located
+		Instantiate(keyPoof, transform.position, Quaternion.identity);
         // Make sure the poof animates vertically
-        // Call the Unlock() method on the Door
-        // Destroy the key. Check the Unity documentation on how to use Destroy
+		// Call the Unlock() method on the Door
+		door.Unlock();
+		// Destroy the key. Check the Unity documentation on how to use Destroy
+		Destroy(gameObject);
     }
 
 }

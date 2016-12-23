9be17782571aa84df84c54b1ec8014f97f813b0a
Susie <susie.odneal@gmail.com>
2016-12-23 10:32:54 -0600

    Door Opens if Key is Destroyed and Work on Sounds
    - Created sound array
    - tried to specify what sounds played when door is locked vs unlocked
    - bah

--- Maze Project/Assets/Udacity/Scripts/Door.cs
+++ Maze Project/Assets/Udacity/Scripts/Door.cs
@@ -7,6 +7,10 @@ public class Door : MonoBehaviour {
     // Create a boolean value called "locked" that can be checked in Update() 
 	private bool locked;
 	private bool raised;
+	public GameObject keyPrefab;
+	public AudioClip[] doorSounds;
+	public AudioSource doorSoundPlayer;
+
 //	private float open = 25;
 
 	void start() {
@@ -17,10 +21,11 @@ public class Door : MonoBehaviour {
 
     void Update() {
         // If the door is unlocked and it is not fully raised
-		if(locked && raised == true) {
+		if (locked && raised == true) {
 			// Animate the door raising up
-				transform.Translate (0, 2.5f * Time.deltaTime, 0);
+			transform.Translate (0, 2.5f * Time.deltaTime, 0);
 
+	
 		}
 	}	
 		
@@ -30,8 +35,15 @@ public class Door : MonoBehaviour {
 		Debug.Log ("Unlock called!");
     }
 
+
 	public void Raise() {
-		raised = true;
+		//if there's no key, the door can be raised
+		if (keyPrefab == null) {
+			raised = true;
+			//doorSoundPlayer.clip = ;
+		}
+			
+
 	Debug.Log ("Raised called!");
 	}
 }
